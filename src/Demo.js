d1eeabe826b69c82a2e05b4db611b8bce2d353ee
Daniel Lerch <daniel@lerchpanther.ch>
2018-05-17 15:46:36 +0200

    added Functionality to fancy new buttons

--- src/Demo.js
+++ src/Demo.js
@@ -1,40 +1,65 @@
 "use strict";
 
+var taskList;
+
 $(function () {
-  TaskList.load('1526560088937700', function (taskList) {
+  if (window.location.hash.length > 16) {
+    TaskList.load(window.location.hash.substring(1), function (tl) {
+      taskList = tl;
+      $('#taskList').html(taskList.render());
+    });
+  } else {
+    taskList = new TaskList();
+    taskList.createTask('');
+    $('#taskList').html(taskList.render());
+  }
+
+  $("#createTask").click(function (event) {
+    event.preventDefault();
+    var task = taskList.createTask("");
+    $("#taskList ul").append(task.render());
+  });
+
+  $("#taskList").on("change", ":checkbox", function () {
+    // all related code goes in here
+    if ($(this).is(":checked")) {
+      $(this)
+        .closest('li')
+        .slideUp("fast", function () {
+          $(this)
+            .addClass("finished")
+            .appendTo("#taskList ul");
+        })
+        .slideDown();
+    } else {
+      $(this)
+        .closest('li')
+        .slideUp("fast", function () {
+          $(this)
+            .removeClass("finished")
+            .prependTo("#taskList ul");
+        })
+        .slideDown();
+    }
+  });
+  $('#saveTasks').click(function (event) {
+    taskList.save();
+  });
+
+  $('#openList').click(function (event) {
+    var listId = prompt("Bitte geben Sie die Tasklisten ID an", "demo");
+    if (listId != null) {
+      TaskList.load(listId, function (taskList) {
+        $('#taskList').html(taskList.render());
+      });
+    }
+  });
+
+  $('#newList').click(function (event) {
+    taskList = new TaskList();
+    taskList.createTask('');
     $('#taskList').html(taskList.render());
 
-    $("#createTask").click(function (event) {
-      event.preventDefault();
-      var task = taskList.createTask("");
-      $("#taskList ul").append(task.render());
-    });
-
-    $("#taskList").on("change", ":checkbox", function () {
-      // all related code goes in here
-      if ($(this).is(":checked")) {
-        $(this)
-          .closest('li')
-          .slideUp("fast", function () {
-            $(this)
-              .addClass("finished")
-              .appendTo("#taskList ul");
-          })
-          .slideDown();
-      } else {
-        $(this)
-          .closest('li')
-          .slideUp("fast", function () {
-            $(this)
-              .removeClass("finished")
-              .prependTo("#taskList ul");
-          })
-          .slideDown();
-      }
-    });
-    $('#saveTasks').click(function (event) {
-      taskList.save();
-    });
   });
 
 });
