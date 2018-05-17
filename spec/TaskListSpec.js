ca5203b86dc88a0e235b18f83411121ca5f8b286
Daniel Lerch <daniel@lerchpanther.ch>
2018-05-17 12:58:50 +0200

    added Tests alle von Lab 9 und 10 übernommen

--- spec/TaskListSpec.js
+++ spec/TaskListSpec.js
@@ -1,31 +1,164 @@
 "use strict";
 
-describe("TaskList", function() {
+describe("TaskList", function () {
   var taskList;
 
-  beforeEach(function() {
+  beforeEach(function () {
     taskList = new TaskList();
   });
 
-  it("adds a new element", function() {
+  it("adds a new element", function () {
     taskList.createTask('test');
     expect(taskList.tasks[0].title).toEqual('test');
   });
 
-  describe("render", function() {
-    it("renders ul element", function() {
+  describe("render", function () {
+    it("renders ul element", function () {
       expect(taskList.render()).toEqual('ul');
     });
 
-    it("renders empty list when empty", function() {
+    it("renders empty list when empty", function () {
       expect(taskList.render()).toBeEmpty();
     });
-    it("renders tasks", function() {
+    it("renders tasks", function () {
       taskList.createTask('test task');
       expect(taskList.render().find('input[name=title]').val()).
-          toBe('test task');
+      toBe('test task');
 
     });
   });
+
+  describe("load", function () {
+    var result;
+
+    beforeEach(function () {
+      // mock the ajax call to the server loading the tasklist
+
+      spyOn($, "getJSON").and.callFAake(function (url, callback) {
+        callback({
+          id: 'demolista',
+          title: 'the demolisteur',
+          tasks: [{
+              title: 'first task',
+              done: true
+            },
+            {
+              title: 'last task',
+              done: false
+            }
+          ]
+        });
+      });
+      // mock ajax call and populate tasklist into result
+      TaskList.load('testlist', function (tasklist) {
+        result = taskList;
+      });
+    });
+
+    it('calls getJSON with the correct URL', function () {
+      expect($.getJSON).toHAveBeenCalledWith("http://zhaw.herokuapp.com/task_lists/testlist", jasmine.any(Function));
+    });
+
+    it("stores the id of the tasklist", function () {
+      expect(result.id).toEqual('demolista');
+    });
+    it("stores the title of the tasklist", function () {
+      expect(result.title).toEqual('the demolisteur');
+    });
+
+    it("creates a tasklist with 2 entries", function () {
+      expect(result.tasks.length).toEqual(2);
+    });
+
+    it("populates the titles of the tasklist", function () {
+      expect(result.tasks[0].title).toEqual('first task');
+      expect(result.tasks[1].title).toEqual('last task');
+    });
+
+    it("populates the done status of the tasks", function () {
+      expect(result.tasks[0].done).toBe(true);
+      expect(result.tasks[1].done).toBe(false);
+    });
+
+  });
+  describe("save (new list)", function () {
+    beforeEach(function () {
+      taskList.id = undefined;
+      taskList.createTask('first');
+      taskList.createTask('second');
+
+      // mock the ajax call to the server persisting the tasklist
+      spyOn($, "post").and.callFake(function (url, data, callback) {
+        callback(JSON.stringify({
+          id: '12345',
+          title: '',
+          tasks: [{
+              title: 'first',
+              done: false
+            },
+            {
+              title: 'second',
+              done: false
+            },
+          ]
+        }));
+      });
+
+      taskList.save();
+    });
+
+    it('calls post with the correct URL', function () {
+      expect($.post).toHaveBeenCalledWith("http://zhaw.herokuapp.com/task_lists/", jasmine.any(String), jasmine.any(Function));
+    });
+
+    it("consumes the id received by the server", function () {
+      expect(taskList.id).toEqual('12345');
+    });
+    // optional exercise
+    it("sets the hash", function () {
+      expect(window.location.hash).toEqual('#12345');
+    });
+  });
+
+
+  describe("save (existing list)", function () {
+    beforeEach(function () {
+      taskList.id = "0815";
+      taskList.createTask('first');
+      taskList.createTask('second');
+
+      // mock the ajax call to the server persisting the tasklist
+      spyOn($, "post").and.callFake(function (url, data, callback) {
+        callback(JSON.stringify({
+          id: '0815',
+          title: '',
+          tasks: [{
+              title: 'first',
+              done: false
+            },
+            {
+              title: 'second',
+              done: false
+            },
+          ]
+        }));
+      });
+
+      taskList.save();
+    });
+
+    it('calls post with the correct URL', function () {
+      expect($.post).toHaveBeenCalledWith("http://zhaw.herokuapp.com/task_lists/0815", jasmine.any(String), jasmine.any(Function));
+    });
+
+    it("consumes the id received by the server", function () {
+      expect(taskList.id).toEqual('0815');
+    });
+    // optional exercise
+    xit("sets the hash", function () {
+      expect(window.location.hash).toEqual('#0815');
+    });
+  });
+
 
 });
